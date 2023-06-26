9adf9ec0d10362c395df4617d2227927d3e89523
wesley mauricio <wesleymauricio85@gmail.com>
2023-06-25 23:01:22 -0300

    alteracoesResponse

--- script.js
+++ script.js
@@ -15,4 +15,12 @@ document.addEventListener("DOMContentLoaded", function() {
       document.getElementById("radio" + count).checked = true;
     }
   });
-  
+function menuShow(){
+  let menuMobile = document.querySelector('.mobileMenu')
+
+  if (menuMobile.classList.contains('open')){
+    menuMobile.classList.remove('open');
+  }else{
+    menuMobile.classList.add('open')
+  }
+}  
